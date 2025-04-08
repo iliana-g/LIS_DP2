e07756635ae2ce6975908c1f9411ae8f1ffefa1c
ili <ilianagranados.05@gmail.com>
2025-04-07 19:20:28 -0600

    Primer Guardado

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<!--todos los recursos visuales en la etiqueta <figure> se usa para contenido como imagenes, videos-->
+    <figure> 
+        <img src="" alt=""/>
+</figure>
+<!--Tip de imagenes: alojarlas en un servidor.
+Por ej: en un trabajo laboral, la empresa tiene el servidor. 
+Para nosotros: drive, guardar las imágenes, cloudinary-->
+</body>
+</html>
